00682895749d24ad1da4c39625a9e2a64949cdee
Mohamed Hedeyaa <68658093+mohamedhadia@users.noreply.github.com>
2020-09-30 06:40:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState, useRef } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(15);
+  const [gameRunning, setGameRunning] = useState(false);
+  const inputRef = useRef(null);
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setText(value);
+  }
+
+  function handleWordCount() {
+    const words = text.trim().split(" ");
+    const filteredwirds = words.filter((word) => word !== "");
+    const numberOfWords = filteredwirds.length;
+    setCount(numberOfWords);
+  }
+
+  useEffect(() => {
+    if ((timer > 0) & (gameRunning === true)) {
+      setTimeout(() => setTimer((time) => time - 1), 1000);
+    } else if (timer === 0) {
+      setGameRunning(false);
+      handleWordCount();
+    }
+  }, [timer, gameRunning]);
+
+  return (
+    <div>
+      <h1>how fast do u type ? </h1>
+      <textarea
+        ref={inputRef}
+        value={text}
+        onChange={handleChange}
+        disabled={!gameRunning}
+      />
+      <h4>time remaining {timer} s</h4>
+      <button
+        disabled={gameRunning}
+        onClick={() => {
+          setGameRunning(true);
+          setTimer(15);
+          setCount(0);
+          setText("");
+          inputRef.current.disabled = false;
+          inputRef.current.focus();
+        }}
+      >
+        start
+      </button>
+      <h2>word count : {count}</h2>
+    </div>
+  );
+}
+
+export default App;
